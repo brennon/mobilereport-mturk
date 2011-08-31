a7fbaf4678813e8c7c8caf0dedb91b9ae3539363
Brennon Bortz <brennon@brennonbortz.com>
2011-08-31 22:53:06 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobilereportMturk::Application.config.secret_token = '3cfd42c81ad8d03a8b2003c5bc5fdf8d613a8efce5926676b93e1cf3dbb74b5248cd4bdbe6522e40f9eaf0a52dc15549487d2802273afce8333ba3a2b0634d5f'
